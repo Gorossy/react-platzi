83221182d58bb79b5811adf89cf8a787fb45c743
David Maldonado <david.maldonado@momseguros.co>
2023-08-13 04:41:37 -0500

    video 3 - Creation of structure todo and components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
-import logo from './platzi.webp';
 import './App.css';
-
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
 function App() {
   return (
     <div className="App">
+      <TodoCounter/>
+      <TodoSearch/>
 
-      <TodoItem/>
+      <TodoList>
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendamos React
-        </a>
-      </header>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+
+      </TodoList>
+
+      {/*<CreateTodoButton/>*/}
     </div>
   );
 }
 
-function TodoItem() {
-  return(
-    <li>
-      <span>V</span>
-      <p>Llorar con la Llorona</p>
-      <span>X</span>
-    </li>
-  );
-}
-export default App;
+  export default App;
