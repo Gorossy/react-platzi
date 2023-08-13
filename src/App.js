bf410ac0829169180b56d0164c12a3e7e0f44018
David Maldonado <david.maldonado@momseguros.co>
2023-08-13 05:07:03 -0500

    Video 4 - Update props of components

--- src/App.js
+++ src/App.js
@@ -4,22 +4,34 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react';
+
+
+const defaultTodos = [
+  {text:"Cortar cebolla", completed:true},
+  {text:"Tomar el curso de react", completed:true},
+  {text:"Cortar papa", completed:false},
+  {text:"Ir a casa", completed:false},
+  {text:"Ir a casa", completed:false},
+]
 function App() {
   return (
-    <div className="App">
-      <TodoCounter/>
+    <React.Fragment>
+      <TodoCounter completed={16} total={25}/>
       <TodoSearch/>
 
       <TodoList>
 
-        <TodoItem/>
-        <TodoItem/>
-        <TodoItem/>
-
+        {defaultTodos.map(todo =>(
+          <TodoItem 
+          key={todo.text} 
+          text={todo.text}
+          completed={todo.completed}/>
+        ))}
       </TodoList>
 
       <CreateTodoButton/>
-    </div>
+    </React.Fragment>
   );
 }
 
